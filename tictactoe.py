39d438e483777f36ab382929cb3d6e6b9fc6adfa
countvk <countvk@hotmail.com>
2016-02-27 23:31:16 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -50,66 +50,83 @@ def play():
 #diagonal checking
                 if m[4]==m[0]and m[4]==m[8]:
                     if m[4]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[4]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
                 if m[4]==m[6]and m[4]==m[2]:
                     if m[4]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[4]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
 #row and column checkings
     #1,1
                 if m[0]==m[1] and m[0]==m[2]:
                     if m[0]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[0]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
                 if m[0]==m[3] and m[0]==m[6]:
                     if m[0]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[0]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
     #2,2
                 if m[3]==m[4] and m[3]==m[5]:
                     if m[4]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[4]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
                 if m[1]==m[4] and m[1]==m[7]:
                     if m[4]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[4]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
     #3,3
                 if m[6]==m[7] and m[6]==m[8]:
                     if m[8]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[8]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
                 if m[2]==m[5] and m[2]==m[8]:
                     if m[8]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[8]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
 #draw match scenario
                 if count==9:
+                    printmat(*m);
                     print("MATCH IS A DRAW!!!!");
                     break;
 #player two's turn
@@ -123,66 +140,82 @@ def play():
                         break;
                     else:
                         print("YOU CANNOT PLACE HERE");
-#checking if
+                #checking if
 #diagonal checking
                 if m[4]==m[0]and m[4]==m[8]:
                     if m[4]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[4]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
                 if m[4]==m[6]and m[4]==m[2]:
                     if m[4]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[4]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
 #row and column checkings
     #1,1
                 if m[0]==m[1] and m[0]==m[2]:
                     if m[0]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[0]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
                 if m[0]==m[3] and m[0]==m[6]:
                     if m[0]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[0]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
     #2,2
                 if m[3]==m[4] and m[3]==m[5]:
                     if m[4]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[4]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
                 if m[1]==m[4] and m[1]==m[7]:
                     if m[4]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[4]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
     #3,3
                 if m[6]==m[7] and m[6]==m[8]:
                     if m[8]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[8]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
                 if m[2]==m[5] and m[2]==m[8]:
                     if m[8]=='x':
+                        printmat(*m);
                         print("PLAYER ONE WINS!!!!!!!");
                         break;
                     if m[8]=='o':
+                        printmat(*m);
                         print("PLAYER TWO WINS!!!!!!!");
                         break;
 
