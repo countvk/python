da5ed98972d5dfe7f3abd29bfe9546ba8230a33d
countvk <countvk@hotmail.com>
2016-02-28 15:32:39 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,226 +1,258 @@
-for p in range(3):
-    print("*\t*\t*\n");
-print("||");
-print("1\t2\t3\n4\t5\t6\n7\t8\t9\n")   ;
-print("enter the respective number to place your symbol player one will be x and player two will be o\n");
-
 import random
 #matrix printer
-
+import tkinter.simpledialog
+import tkinter.messagebox
 def printmat(*m):
-    for i in range(3):
-        print(m[i],end='');
+    for i in range(0,3):
+
+        print(  m[i]  ,end='');
+        print(  "\t" ,end='');
     print("\n");
     for i in range(3,6):
-        print(m[i],end='');
+
+        print(  m[i]  ,end='');
+        print(  "\t" ,end='');
     print("\n");
     for i in range(6,9):
-        print(m[i],end='');
+
+        print(  m[i]  ,end='');
+        print(  "\t" ,end='');
     print("\n");
+#labeler
+    for i in range(0,3):
 
+        t.insert(END, m[i] + '\t')
+    t.insert(END,  '\n')
+    for i in range(3,6):
+
+       t.insert(END, m[i] + '\t')
+    t.insert(END,  '\n')
+    for i in range(6,9):
+
+       t.insert(END, m[i] + '\t')
+    t.insert(END,  '\n')
+    t.insert(END,'_______________________\n')
+
+
+
+
+#play button function
 def play():
-
-
-
     while 1:
-        #game asker
+        print("new game starts")
+        m=['*','*','*','*','*','*','*','*','*'];
+        count=0;
 
-        print("NEW GAME=any number AND QUIT=0");
-#turn rotation while loop
-        ans=int(input());
-        if ans!=0:
-            m=['*','*','*','*','*','*','*','*','*'];
-            count=0;
-            while 1:
+        while 1:
 
 
 #player one's turn
 
-                while 1:
-                    print("PLAYER ONE");
-                    printmat(*m);
-                    x=int(input())-1;
-                    if m[x]!='x' and m[x]!='o':
+            while 1:
+                print("PLAYER ONE");
+
+                printmat(*m)
+                x=int(tkinter.simpledialog.askinteger('playerone','number'))-1;
+                if m[x]!='x' and m[x]!='o':
                         m[x]='x';
                         count+=1;
                         break;
-                    else:
-                        print("YOU CANNOT PLACE HERE");
+                else:
+                    tkinter.messagebox.showerror('!!!!!!!!','UCANTDOTHAT!')
+
 #checking if
 #diagonal checking
-                if m[4]==m[0]and m[4]==m[8]:
-                    if m[4]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[4]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-                if m[4]==m[6]and m[4]==m[2]:
-                    if m[4]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[4]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-#row and column checkings
-    #1,1
-                if m[0]==m[1] and m[0]==m[2]:
-                    if m[0]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[0]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-                if m[0]==m[3] and m[0]==m[6]:
-                    if m[0]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[0]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-    #2,2
-                if m[3]==m[4] and m[3]==m[5]:
-                    if m[4]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[4]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-                if m[1]==m[4] and m[1]==m[7]:
-                    if m[4]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[4]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-    #3,3
-                if m[6]==m[7] and m[6]==m[8]:
-                    if m[8]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[8]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-                if m[2]==m[5] and m[2]==m[8]:
-                    if m[8]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[8]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-#draw match scenario
-                if count==9:
+            if m[4]==m[0]and m[4]==m[8]:
+                if m[4]=='x':
                     printmat(*m);
-                    print("MATCH IS A DRAW!!!!");
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
                     break;
-#player two's turn
-                while 1:
-                    print("PLAYER TWO");
+                if m[4]=='o':
                     printmat(*m);
-                    y=random.rndint(1,9)-1;
-                    if m[y]!='x' and m[y]!='o':
-                        m[y]='o';
-                        count+=1;
-                        break;
-                    else:
-                        print("YOU CANNOT PLACE HERE");
-                #checking if
-#diagonal checking
-                if m[4]==m[0]and m[4]==m[8]:
-                    if m[4]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[4]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-                if m[4]==m[6]and m[4]==m[2]:
-                    if m[4]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[4]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+            if m[4]==m[6]and m[4]==m[2]:
+                if m[4]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[4]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
 #row and column checkings
     #1,1
-                if m[0]==m[1] and m[0]==m[2]:
-                    if m[0]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[0]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-                if m[0]==m[3] and m[0]==m[6]:
-                    if m[0]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[0]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
+            if m[0]==m[1] and m[0]==m[2]:
+                if m[0]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[0]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+            if m[0]==m[3] and m[0]==m[6]:
+                if m[0]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[0]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
     #2,2
-                if m[3]==m[4] and m[3]==m[5]:
-                    if m[4]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[4]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-                if m[1]==m[4] and m[1]==m[7]:
-                    if m[4]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[4]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
+            if m[3]==m[4] and m[3]==m[5]:
+                if m[4]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[4]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+            if m[1]==m[4] and m[1]==m[7]:
+                if m[4]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[4]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
     #3,3
-                if m[6]==m[7] and m[6]==m[8]:
-                    if m[8]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[8]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
-                if m[2]==m[5] and m[2]==m[8]:
-                    if m[8]=='x':
-                        printmat(*m);
-                        print("PLAYER ONE WINS!!!!!!!");
-                        break;
-                    if m[8]=='o':
-                        printmat(*m);
-                        print("PLAYER TWO WINS!!!!!!!");
-                        break;
+            if m[6]==m[7] and m[6]==m[8]:
+                if m[8]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[8]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+            if m[2]==m[5] and m[2]==m[8]:
+                if m[8]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[8]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+#draw match scenario
+            if count==9:
+                printmat(*m);
+                tkinter.messagebox.showinfo('!!!!!!!!','ITISADRAW')
+                break;
+#player two's turn
+            while 1:
+                print("wait ...........");
 
-        else:
-            print("GOOD BYE");
-            break;
-#play
-play();
+                y=random.randint(1,9)-1;
+                if m[y]!='x' and m[y]!='o':
+                    m[y]='o';
+                    count+=1;
+                    break;
+                else:
+                    print("YOU CANNOT PLACE HERE");
+#checking if
+#diagonal checking
+            if m[4]==m[0]and m[4]==m[8]:
+                if m[4]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[4]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+            if m[4]==m[6]and m[4]==m[2]:
+                if m[4]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[4]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+#row and column checkings
+    #1,1
+            if m[0]==m[1] and m[0]==m[2]:
+                if m[0]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[0]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+            if m[0]==m[3] and m[0]==m[6]:
+                if m[0]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[0]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+    #2,2
+            if m[3]==m[4] and m[3]==m[5]:
+                if m[4]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[4]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+            if m[1]==m[4] and m[1]==m[7]:
+                if m[4]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[4]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+    #3,3
+            if m[6]==m[7] and m[6]==m[8]:
+                if m[8]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[8]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+            if m[2]==m[5] and m[2]==m[8]:
+                if m[8]=='x':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','UWON')
+                    break;
+                if m[8]=='o':
+                    printmat(*m);
+                    tkinter.messagebox.showinfo('!!!!!!!!','ULOST')
+                    break;
+        tkinter.messagebox.showinfo('matchfinished','hitplayfornewmatch')
+        break;
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+from tkinter import *
+
+master = Tk()
+master.geometry()
+t=Text(master)
+t.pack()
+master.title("XNO BY VINAY")
+frame=Frame(master,width=1000,height=500)
+frame.pack()
+tkinter.messagebox.showinfo("INSTRUCTIONS",'keeping in mind that each position is respectively held by a number from 1-9\n give the respective number to place your pick\n the first row elements are 1,2,3 respectively\n')
+tlabel=Label(master,text="TIC TAC TOE")
+tlabel.pack(side="top")
+but1=Button(frame,text="play",fg="red",bg="yellow",command=play)
+but1.pack(side="left",fill=X,expand=YES)
+but2=Button(master,text="quit!",fg="orange",bg="black",command=frame.quit)
+but2.pack(side="bottom",fill=X,expand=YES)
+
+
+
+master.mainloop()
